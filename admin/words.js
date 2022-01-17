5ca959bb62778638e9bf4b7759d2523aba4dd047
Harald <harald@lobo.local>
2022-01-17 17:43:18 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,53 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+    "epson-esc-vp21 adapter settings": {
+        "en": "Adapter settings for epson-esc-vp21",
+        "de": "Adaptereinstellungen für epson-esc-vp21",
+        "ru": "Настройки адаптера для epson-esc-vp21",
+        "pt": "Configurações do adaptador para epson-esc-vp21",
+        "nl": "Adapterinstellingen voor epson-esc-vp21",
+        "fr": "Paramètres d'adaptateur pour epson-esc-vp21",
+        "it": "Impostazioni dell'adattatore per epson-esc-vp21",
+        "es": "Ajustes del adaptador para epson-esc-vp21",
+        "pl": "Ustawienia adaptera dla epson-esc-vp21",
+        "zh-cn": "epson-esc-vp21的适配器设置"
+    },
+    "net_address": {
+        "en": "Network address",
+        "de": "Netzwerkadresse",
+        "ru": "Сетевой адрес",
+        "pt": "Endereço de rede",
+        "nl": "Netwerkadres",
+        "fr": "Adresse réseau",
+        "it": "Indirizzo di rete",
+        "es": "Dirección de red",
+        "pl": "Adres sieciowy",
+        "zh-cn": "网络地址"
+    },
+    "port_number": {
+        "en": "Port number",
+        "de": "Port-Nummer",
+        "ru": "Номер порта",
+        "pt": "Número da porta",
+        "nl": "Poortnummer",
+        "fr": "Numéro de port",
+        "it": "Numero di porta",
+        "es": "Número de puerto",
+        "pl": "Numer portu",
+        "zh-cn": "端口号"
+    },
+    "poll_intervall": {
+        "en": "Poll intervall",
+        "de": "Abfrageintervall",
+        "ru": "Интервал опроса",
+        "pt": "Intervalo de sondagem",
+        "nl": "Poll-interval",
+        "fr": "Intervalle d'interrogation",
+        "it": "Intervallo sondaggio",
+        "es": "Intervalo de encuesta",
+        "pl": "Interwał sondowania",
+        "zh-cn": "轮询间隔"
+    }
+};
